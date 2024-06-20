d502505e2e18a09f519e5969f8f652b9c5ead7f3
ProtacioUno <enervinluan01@gmail.com>
2024-06-20 15:20:38 +0800

    001 certs

--- src/main.js
+++ src/main.js
@@ -11,32 +11,69 @@
 //   console.log('Viewport width is greater than 600px');
 // }
 
-const mediaQuery = window.matchMedia('(max-width: 1000px)');
+let mediaQuery = window.matchMedia('(max-width: 1000px)');
 let profile_card = document.querySelector('.profile-card');
+// let hide_me = document.querySelectorAll('.hide-me');
 
 // Function to handle changes in the media query
-const handleMediaQueryChange = (mediaQueryList) => {
+let handleMediaQueryChange = (mediaQueryList) => {
   if (mediaQueryList.matches) {
     // Media query matches (viewport width is 600px or less)
-    console.log('Viewport width is now 600px or less');
-
+    // console.log('Viewport width is now 600px or less');
+    
+    
     profile_card.setAttribute('hidden' , 'hidden');
+    
+    // hide_me.forEach(hides =>{
+    //   hides.setAttribute('hidden' , 'hidden');
+
+    // })
 
     
     // Add your responsive behavior here, such as hiding/showing elements, adjusting layout, etc.
   } else {
     // Media query doesn't match (viewport width is greater than 600px)
-    console.log('Viewport width is now greater than 600px'); 
+    // console.log('Viewport width is now greater than 600px'); 
     profile_card.removeAttribute('hidden');
     // Add your responsive behavior here
   } 
 };
 
-// Initial check if the media query matches
 handleMediaQueryChange(mediaQuery);
-
-// Add a listener for changes in the media query
 mediaQuery.addListener(handleMediaQueryChange);
+
+let mediaQuery_smol = window.matchMedia('(max-width: 500px)');
+let hide_me = document.querySelectorAll('.hide-me');
+
+// Function to handle changes in the media query
+let handleMediaQueryChange_smol = (mediaQueryList) => {
+  if (mediaQueryList.matches) {
+    // Media query matches (viewport width is 600px or less)
+    console.log('Viewport width is now 500px or less');
+    
+    
+    
+    hide_me.forEach(hides =>{
+      hides.setAttribute('hidden' , 'hidden');
+    })
+
+    
+  } else {
+    // Media query doesn't match (viewport width is greater than 600px)
+    console.log('Viewport width is now greater than 600px'); 
+    // profile_card.removeAttribute('hidden');
+  } 
+};
+
+
+handleMediaQueryChange_smol(mediaQuery_smol);
+mediaQuery_smol.addListener(handleMediaQueryChange_smol);
+
+
+
+
+
+
 
 
 
